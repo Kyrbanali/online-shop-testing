32e5d46f9f7b8da6ae95540effea8df9e5fdb549
Kurbanali <arslanaliev.ruby@gmail.com>
2024-01-18 15:13:03 +0300

    защита от sql инъекции

--- app/public/post_registrate.php
+++ app/public/post_registrate.php
@@ -8,5 +8,5 @@ $passwordRepeat = $_POST['psw-repeat'];
 
 $pdo = new PDO("pgsql:host=db;port=5432;dbname=dbtest", "dbuser", "dbpwd");
 
-
-$pdo->exec("INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')");
+$stmt = $pdo->prepare('INSERT INTO users (name, email, password) VALUES (:name, :email, :password)');
+$stmt->execute(['name' => $name, 'email' => $email, 'password' => $password]);
