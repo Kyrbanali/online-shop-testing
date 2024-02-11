fc9b2dbce138cdcc29ff0a5a2752ad38bc693b48
Kurbanali <arslanaliev.ruby@gmail.com>
2024-02-11 22:40:29 +0300

    +hydrate()

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -18,53 +18,6 @@ class Product extends Model
 
     }
 
-    public static function getAll() : ?array
-    {
-        $sql = 'SELECT * FROM products';
-
-        $stmt = self::getPDO()->query($sql);
-        $products = $stmt->fetchAll();
-
-        foreach ($products as $product) {
-            $data[] = new Product($product['id'], $product['name'], $product['description'], $product['price'], $product['img_url']);
-        }
-
-        if (empty($data)) {
-            return null;
-        }
-        return $data;
-    }
-
-    public static function getAllByIds(array $productIds) : ?array
-    {
-        $string = implode(", ", $productIds);
-        $sql = <<<SQL
-                SELECT * FROM products WHERE id IN ($string)
-                SQL;
-        $stmt = self::getPdo()->query($sql);
-        $products = $stmt->fetchAll();
-
-        foreach ($products as $product) {
-            $data[$product['id']] = new Product($product['id'], $product['name'], $product['description'], $product['price'], $product['img_url']);
-        }
-
-        if (empty($data)) {
-            return null;
-        }
-
-        return $data;
-    }
-
-    public function getOneById(int $productId)
-    {
-        $sql = 'SELECT * FROM products where id = :product_id';
-
-        $data = ['product_id' => $productId];
-
-        $stmt = self::prepareExecute($sql, $data);
-        $result = $stmt->fetch();
-    }
-
     public function getId(): int
     {
         return $this->id;
@@ -88,6 +41,77 @@ class Product extends Model
     public function getImgUrl(): string
     {
         return $this->img_url;
+    }
+
+    public static function getAll() : ?array
+    {
+        $sql = 'SELECT * FROM products';
+
+        $stmt = self::getPDO()->query($sql);
+        $products = $stmt->fetchAll();
+
+        $data = self::hydrate($products);
+
+
+        if (empty($data)) {
+            return null;
+        }
+        return $data;
+    }
+
+    public static function getAllByIds(array $productIds) : ?array
+    {
+        $string = implode(", ", $productIds);
+        $sql = <<<SQL
+                SELECT * FROM products WHERE id IN ($string)
+                SQL;
+        $stmt = self::getPdo()->query($sql);
+        $products = $stmt->fetchAll();
+
+        $data = self::hydrate($products, true);
+
+        if (empty($data)) {
+            return null;
+        }
+
+        return $data;
+    }
+
+    public static function hydrate(array $products, bool $associative = false): ?array
+    {
+        $data = [];
+
+        foreach ($products as $product) {
+            if ($associative) {
+                $data[$product['id']] = new Product(
+                    $product['id'],
+                    $product['name'],
+                    $product['description'],
+                    $product['price'],
+                    $product['img_url']
+                );
+            } else {
+                $data[] = new Product(
+                    $product['id'],
+                    $product['name'],
+                    $product['description'],
+                    $product['price'],
+                    $product['img_url']
+                );
+            }
+        }
+
+        return $data;
+    }
+
+    public function getOneById(int $productId)
+    {
+        $sql = 'SELECT * FROM products where id = :product_id';
+
+        $data = ['product_id' => $productId];
+
+        $stmt = self::prepareExecute($sql, $data);
+        $result = $stmt->fetch();
     }
 
     public function getQuantity(int $userId) : int
